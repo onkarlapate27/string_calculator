9207cc0b79fd779547485e3dced4cd94420178f0
onkarlapate27 <onkar.lapate040@gmail.com>
2025-03-22 16:11:29 +0530

    Refactored code to simply logic and remove redundant code.

--- str_calculator.py
+++ str_calculator.py
@@ -8,26 +8,20 @@ def add(nums: str) -> int:
 
         # Check for custom delimiter in format "//[delimiter]\n[numbers…]"
         if nums.startswith("//"):
-            split_str = nums.split('\n', 1)
-            delimiter = split_str[0][2:]
-            nums = split_str[1]
+            delimiter_part, nums = nums.split('\n', 1)
+            delimiter = delimiter_part[2:]
 
         # Normalize delimiters
-        nums = nums.replace('\n', ',')
-        nums = nums.replace(delimiter, ',')
+        nums = nums.replace('\n', ',').replace(delimiter, ',')
 
-        int_numbers_list = [int(num.strip()) for num in nums.split(',')]
-        negative_numbers_list = [num for num in int_numbers_list if num < 0]
+        numbers_list = [int(num.strip()) for num in nums.split(',')]
+        negative_numbers_list = [num for num in numbers_list if num < 0]
 
-        if len(negative_numbers_list) > 0:
-            negative_nums_str = str(negative_numbers_list[0])
-            if len(negative_numbers_list) > 1:
-                for num in range(1, len(negative_numbers_list)):
-                    negative_nums_str += f', {str(negative_numbers_list[num])}'
+        if negative_numbers_list:
+            negatives_str = ', '.join(str(num) for num in negative_numbers_list)
+            raise ValueError(f"negative numbers not allowed {negatives_str}")
 
-            raise ValueError(f"negative numbers not allowed {negative_nums_str}")
-
-        return sum(int_numbers_list)
+        return sum(numbers_list)
     
     except Exception as e:
         print("Issue in calculating the string resultant.", e)
