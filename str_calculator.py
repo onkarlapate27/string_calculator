548a082b35fbd63f2d99c1e8e8b4e21e1cba71bb
onkarlapate27 <onkar.lapate040@gmail.com>
2025-03-22 13:06:07 +0530

    Refactored code to accomodate numbers with whitespaces.

--- str_calculator.py
+++ str_calculator.py
@@ -2,7 +2,8 @@ def add(nums: str) -> int:
     # empty string
     if not nums:
         return 0
-    # n comma separated numbers
+    # n numbers
     else:
-        int_numbers_list = [int(num) for num in nums.split(',')]
+        nums = nums.replace('\n', ',')
+        int_numbers_list = [int(num.strip()) for num in nums.split(',')]
         return sum(int_numbers_list)
--- test_str_calculator.py
+++ test_str_calculator.py
@@ -11,3 +11,9 @@ def test_double_digits_str_input():
 
 def test_n_digits_str_input():
     assert add("2, 7, 4, 6, 7, 8") == 34
+
+def test_digits_with_spaces_input():
+    assert add("     2, 7,     10") == 19
+
+def test_digits_separated_by_newline_char_input():
+    assert add("2, 7, 8\n 8") == 25
